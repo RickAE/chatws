9100c0518f615637d4e41c2c0d081852ff03c932
Ricardo Santos Ferreira <ricardo.santos.ferreira29@gmail.com>
2024-08-03 13:38:20 -0300

    Primeiro commit do projeto. Criando estrutura básica com o spring initalizer

---
+++ src/test/java/com/ricardo/chatws/ChatwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.chatws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
